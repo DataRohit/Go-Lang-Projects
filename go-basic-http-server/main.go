dece257cc20c05616885220f055aaca3e72cdf1e
DataRohit <rohit.vilas.ingole@gmail.com>
2024-08-26 21:13:13 +0530

    project 1 - feature: /hello route and helloHandler added

--- go-basic-http-server/main.go
+++ go-basic-http-server/main.go
@@ -6,9 +6,26 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	if _, err := fmt.Fprintf(w, "Hello World!"); err != nil {
+		return
+	}
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("Server is listening at :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
