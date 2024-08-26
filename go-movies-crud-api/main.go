ca03faef3303c8d5433fe3755042f0820634201a
DataRohit <rohit.vilas.ingole@gmail.com>
2024-08-26 21:21:31 +0530

    project 2 - feature: mux router added

---
+++ go-movies-crud-api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+}
