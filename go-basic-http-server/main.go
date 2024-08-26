9dba9551180ab36c2840ef3d8291c1d6dde3ef30
DataRohit <rohit.vilas.ingole@gmail.com>
2024-08-26 21:13:40 +0530

    project 1 - feature: /form route and formHandler added

--- go-basic-http-server/main.go
+++ go-basic-http-server/main.go
@@ -22,10 +22,32 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	if _, err := fmt.Fprintf(w, "POST request successful\n"); err != nil {
+		return
+	}
+
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	if _, err := fmt.Fprintf(w, "Name = %s\n", name); err != nil {
+		return
+	}
+	if _, err := fmt.Fprintf(w, "Address = %s\n", address); err != nil {
+		return
+	}
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Println("Server is listening at :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
