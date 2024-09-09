a4af0627e24475f0ead2a65d621bbc63522428db
DataRohit <rohit.vilas.ingole@gmail.com>
2024-09-09 08:10:46 +0530

    project 8 - feature: database config added

--- go-jwt-auth-project/database/database.go
+++ go-jwt-auth-project/database/database.go
@@ -1,1 +1,36 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var connectionURI string = os.Getenv("MONGODB_URI")
+var databaseName string = os.Getenv("DATABASE_NAME")
+
+func DatabaseInstance() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb!")
+
+	return client
+}
+
+var Client *mongo.Client = DatabaseInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database(databaseName).Collection(collectionName)
+	return collection
+}
--- go-jwt-auth-project/go.mod
+++ go-jwt-auth-project/go.mod
@@ -8,6 +8,17 @@ require (
 )
 
 require (
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/sync v0.7.0 // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
