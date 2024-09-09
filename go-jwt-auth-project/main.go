dada3d6574d923cd1676d41988285100daac0314
DataRohit <rohit.vilas.ingole@gmail.com>
2024-09-09 07:56:29 +0530

    project 8 - feature: .env setup and gin router created

--- go-jwt-auth-project/main.go
+++ go-jwt-auth-project/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("error loading the .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.GET("/api/v1", func(ginCtx *gin.Context) {
+		ginCtx.JSON(http.StatusOK, gin.H{"success": "access granted for /api/v1"})
+	})
+
+	router.GET("/api/v2", func(ginCtx *gin.Context) {
+		ginCtx.JSON(http.StatusOK, gin.H{"success": "access granted for /api/v2"})
+	})
+
+	router.Run(fmt.Sprintf(":%s", port))
+}
